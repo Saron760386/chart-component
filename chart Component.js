26f90bda42ab4e0bd4612c3cdc88f98ecfeed0c9
saronmeoww <130036298+Saron760386@users.noreply.github.com>
2023-12-16 22:43:27 -0800

    Add files via upload

---
+++ chart Component.js
@@ -0,0 +1,12 @@
+const hoverclick=document.querySelector(".wednesday1")
+const color=document.querySelector(".wed")
+
+hoverclick.addEventListener('click',()=>{
+    color.style.display="block";
+})
+const clickhover=document.querySelector(".thusday1")
+const colorcontent=document.querySelector(".thu")
+
+clickhover.addEventListener('click',()=>{
+    colorcontent.style.display="block";
+})
